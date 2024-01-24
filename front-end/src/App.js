d4e9fb5991bf9f8c8268d45bbc631352002c516e
ujjwlkr01 <99142201+ujjwlkr01@users.noreply.github.com>
2024-01-24 13:08:33 +0530

    new components added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav />
+        <Routes>
+          <Route path="/" element={<h1>Homepage</h1>} />
+          <Route path="/add" element={<h1>AddProduct Page</h1>} />
+          <Route path="/update" element={<h1>UpdateProduct Page</h1>} />
+          <Route path="/logout" element={<h1>Logout Page</h1>} />
+          <Route path="/profile" element={<h1>Profile Page</h1>} />
+        </Routes>
+      </BrowserRouter>
+      {/* <h1>E-commerce Dashboard</h1> */}
+      <Footer />
     </div>
   );
 }
---
+++ front-end/src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+  return (
+    <div className="footer">
+      <h3>E-Comm Dashboard</h3>
+    </div>
+  );
+};
+export default Footer;
